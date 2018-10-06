a2fcc2bd494a122f333f09854fe6dbafe857574e
daisuke310vvv <daisuke.sato1991@gmail.com>
2018-10-07 02:35:29 +0900

    Change entity type

--- transaction.go
+++ transaction.go
@@ -1,14 +1,19 @@
 package getho
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"strconv"
+)
 
 type Transaction struct {
-	From     *string `json:"from"`
-	Gas      *string `json:"gas"`
-	GasPrice *string `json:"gasPrice"`
-	To       *string `json:"to"`
-	Value    *string `json:"value"`
-	Data     *string `json:"data"`
+	From     *string  `json:"from"`
+	Gas      *uint64  `json:"gas"`
+	GasPrice *big.Int `json:"gasPrice"`
+	To       *string  `json:"to"`
+	Value    *big.Int `json:"value"`
+	Data     *string  `json:"data"`
 }
 
 func (tx *Transaction) toMap() map[string]interface{} {
@@ -19,11 +24,11 @@ func (tx *Transaction) toMap() map[string]interface{} {
 	}
 
 	if tx.Gas != nil {
-		txMap["gas"] = *tx.Gas
+		txMap["gas"] = fmt.Sprintf("0x%s", toHexString(int(*tx.Gas)))
 	}
 
 	if tx.GasPrice != nil {
-		txMap["gasPrice"] = *tx.GasPrice
+		txMap["gasPrice"] = fmt.Sprintf("0x%0x", tx.GasPrice)
 	}
 
 	if tx.To != nil {
@@ -31,7 +36,7 @@ func (tx *Transaction) toMap() map[string]interface{} {
 	}
 
 	if tx.Value != nil {
-		txMap["value"] = *tx.Value
+		txMap["value"] = fmt.Sprintf("0x%0x", tx.Value)
 	}
 
 	if tx.Data != nil {
@@ -46,38 +51,113 @@ func (tx *Transaction) toMap() map[string]interface{} {
 }
 
 func (tx *Transaction) String() string {
-	return fmt.Sprintf("from: %v\nto: %v\ngas: %v\ngasPrice: %v\nvalue: %v\ndata: %v\n", tx.From, tx.To, tx.Gas, tx.GasPrice, tx.Value, tx.Data)
+	return fmt.Sprintf("from: %v\nto: %v\ngas: %v\ngasPrice: %v\nvalue: %v\ndata: %v\n",
+		getString(tx.From),
+		getString(tx.To),
+		getUint64(tx.Gas),
+		tx.GasPrice.String(),
+		tx.Value.String(),
+		getString(tx.Data),
+	)
 }
 
 type RawTransaction struct {
 	Transaction
-	BlockHash        string  `json:"blockHash"`
-	BlockNumber      *string `json:"blockNumber"`
-	Hash             string  `json:"hash"`
-	Input            string  `json:"input"`
-	Nonce            string  `json:"nonce"`
-	R                string  `json:"r"`
-	S                string  `json:"s"`
-	TransactionIndex string  `json:"transactionIndex"`
-	V                string  `json:"v"`
+	BlockHash        string   `json:"blockHash"`
+	BlockNumber      *big.Int `json:"blockNumber"`
+	Hash             string   `json:"hash"`
+	Input            string   `json:"input"`
+	Nonce            uint64   `json:"nonce"`
+	R                *big.Int `json:"r"`
+	S                *big.Int `json:"s"`
+	TransactionIndex *uint64  `json:"transactionIndex"`
+	V                *big.Int `json:"v"`
 }
 
 func (tx *RawTransaction) String() string {
 	return fmt.Sprintf("from: %v\nto: %v\ngas: %v\ngasPrice: %v\nvalue: %v\ndata: %v\nblockHash: %v\nblockNumber: %v\nhash: %v\ninput: %v\nnounce: %v\nr: %v\ns: %v\nv: %v\ntransactionIndex: %v\n",
-		tx.From,
-		tx.To,
-		tx.Gas,
-		tx.GasPrice,
-		tx.Value,
-		tx.Data,
+		getString(tx.From),
+		getString(tx.To),
+		getUint64(tx.Gas),
+		tx.GasPrice.String(),
+		tx.Value.String(),
+		getString(tx.Data),
 		tx.BlockHash,
-		tx.BlockNumber,
+		tx.BlockNumber.String(),
 		tx.Hash,
 		tx.Input,
 		tx.Nonce,
-		tx.R,
-		tx.S,
-		tx.V,
-		tx.TransactionIndex,
+		tx.R.String(),
+		tx.S.String(),
+		tx.V.String(),
+		getUint64(tx.TransactionIndex),
 	)
+}
+
+func (t *RawTransaction) UnmarshalJSON(data []byte) error {
+	type Alias RawTransaction
+	a := &struct {
+		Gas              *string `json:"gas"`
+		GasPrice         *string `json:"gasPrice"`
+		Value            *string `json:"value"`
+		BlockNumber      *string `json:"blockNumber"`
+		Nonce            *string `json:"nonce"`
+		R                *string `json:"r"`
+		S                *string `json:"s"`
+		TransactionIndex *string `json:"transactionIndex"`
+		V                *string `json:"v"`
+		*Alias
+	}{
+		Alias: (*Alias)(t),
+	}
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	if a.Gas != nil {
+		gas, _ := strconv.ParseUint(remove0x(*a.Gas), 16, 64)
+		t.Gas = &gas
+	}
+
+	if a.GasPrice != nil {
+		gasPrice, _ := new(big.Int).SetString(remove0x(*a.GasPrice), 16)
+		t.GasPrice = gasPrice
+	}
+
+	if a.Value != nil {
+		value, _ := new(big.Int).SetString(remove0x(*a.Value), 16)
+		t.Value = value
+	}
+
+	if a.BlockNumber != nil {
+		blkNumber, _ := new(big.Int).SetString(remove0x(*a.BlockNumber), 16)
+		t.BlockNumber = blkNumber
+	}
+
+	if a.Nonce != nil {
+		nonce, _ := strconv.ParseUint(remove0x(*a.Nonce), 16, 64)
+		t.Nonce = nonce
+	}
+
+	if a.R != nil {
+		r, _ := new(big.Int).SetString(remove0x(*a.R), 16)
+		t.R = r
+	}
+
+	if a.S != nil {
+		s, _ := new(big.Int).SetString(remove0x(*a.S), 16)
+		t.S = s
+	}
+
+	if a.V != nil {
+		v, _ := new(big.Int).SetString(remove0x(*a.V), 16)
+		t.V = v
+	}
+
+	if a.TransactionIndex != nil {
+		txIndex, _ := strconv.ParseUint(remove0x(*a.TransactionIndex), 16, 64)
+		t.TransactionIndex = &txIndex
+	}
+
+	return nil
 }
--- transaction_receipt.go
+++ transaction_receipt.go
@@ -1,17 +1,61 @@
 package getho
 
+import (
+	"encoding/json"
+	"math/big"
+	"strconv"
+)
+
 type TransactionReceipt struct {
 	BlockHash         *string     `json:"blockHash"`
-	BlockNumber       *string     `json:"blockNumber"`
+	BlockNumber       *big.Int    `json:"blockNumber"`
 	ContractAddress   *string     `json:"contractAddress"`
-	CumulativeGasUsed *string     `json:"cumulativeGasUsed"`
+	CumulativeGasUsed *uint64     `json:"cumulativeGasUsed"`
 	From              string      `json:"from"`
-	GasUsed           string      `json:"gasUsed"`
+	GasUsed           uint64      `json:"gasUsed"`
 	Logs              interface{} `json:"logs"`
 	LogsBloom         string      `json:"logsBloom"`
 	To                *string     `json:"to"`
 	TransactionHash   string      `json:"transactionHash"`
-	TransactionIndex  *string     `json:"transactionIndex"`
+	TransactionIndex  *uint64     `json:"transactionIndex"`
 	Root              *string     `json:"root"`
 	Status            string      `json:"status"`
+}
+
+func (r *TransactionReceipt) UnmarshalJSON(data []byte) error {
+	type Alias TransactionReceipt
+	a := &struct {
+		BlockNumber       *string `json:"blockNumber"`
+		CumulativeGasUsed *string `json:"cumulativeGasUsed"`
+		GasUsed           *string `json:"gasUsed"`
+		TransactionIndex  *string `json:"transactionIndex"`
+		*Alias
+	}{
+		Alias: (*Alias)(r),
+	}
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	if a.BlockNumber != nil {
+		blkNumber, _ := new(big.Int).SetString(remove0x(*a.BlockNumber), 16)
+		r.BlockNumber = blkNumber
+	}
+
+	if a.CumulativeGasUsed != nil {
+		cumulativeGasUsed, _ := strconv.ParseUint(remove0x(*a.CumulativeGasUsed), 16, 64)
+		r.CumulativeGasUsed = &cumulativeGasUsed
+	}
+
+	if a.GasUsed != nil {
+		gasUsed, _ := strconv.ParseUint(remove0x(*a.GasUsed), 16, 64)
+		r.GasUsed = gasUsed
+	}
+
+	if a.TransactionIndex != nil {
+		txIndex, _ := strconv.ParseUint(remove0x(*a.TransactionIndex), 16, 64)
+		r.TransactionIndex = &txIndex
+	}
+
+	return nil
 }
